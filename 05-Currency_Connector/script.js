4a9b1c9dd7d79da5c2f0e1cd91989fc1d69af5d1
Halil Y. <haliilyesilyurt@gmail.com>
2024-01-31 21:26:35 +0300

    Add variable declerations

---
+++ 05-Currency_Connector/script.js
@@ -0,0 +1,11 @@
+const currencyOne = document.getElementById('currency-one');
+const currencyTwo = document.getElementById('currency-two');
+const amountOne = document.getElementById('amount-one');
+const amountTwo = document.getElementById('amount-two');
+const rate = document.getElementById('rate');
+const swap = document.getElementById('swap');
+
+// Fetch the current exchanges rates and update the DOM
+function calculate() {}
+
+calculate();
