950279d07e3e288653f10e50d824ffc9385b6abd
Halil Y. <haliilyesilyurt@gmail.com>
2024-01-31 21:49:11 +0300

    Add function to calculate rate and fetch api

--- 05-Currency_Connector/script.js
+++ 05-Currency_Connector/script.js
@@ -2,10 +2,28 @@ const currencyOne = document.getElementById('currency-one');
 const currencyTwo = document.getElementById('currency-two');
 const amountOne = document.getElementById('amount-one');
 const amountTwo = document.getElementById('amount-two');
-const rate = document.getElementById('rate');
-const swap = document.getElementById('swap');
+const rateEl = document.getElementById('rate');
+const swapEl = document.getElementById('swap');
 
 // Fetch the current exchanges rates and update the DOM
-function calculate() {}
+function calculate() {
+  const currencyOneVal = currencyOne.value;
+  const currencyTwoVal = currencyTwo.value;
+
+  fetch(`https://open.er-api.com/v6/latest/${currencyOneVal}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const rate = data.rates[currencyTwoVal];
+      rateEl.innerText = ` 1 ${currencyOneVal} = ${rate} ${currencyTwoVal}`;
+
+      amountTwo.value = (amountOne.value * rate).toFixed(4);
+    });
+}
+
+// Event listeners for any change
+currencyOne.addEventListener('change', calculate);
+currencyTwo.addEventListener('change', calculate);
+amountOne.addEventListener('input', calculate);
+amountTwo.addEventListener('input', calculate);
 
 calculate();
